421be9e7c9d103effe98607b606faab67a1586f3
sunnylalwani41 <sunnylalwani41@gmail.com>
2026-03-08 13:14:08 +0530

    change the table name

--- src/main/java/com/vivatechrnd/model/User.java
+++ src/main/java/com/vivatechrnd/model/User.java
@@ -14,9 +14,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Pattern;
 
 @Entity
+@Table(name = "users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
